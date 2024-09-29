31bd6ac6280e2aa38d19007302be3d5a24415361
007Shawon <shawon120744@gmail.com>
2024-09-29 19:23:15 +0600

    review count and average rating count done

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('books.index');
 });
 Route::resource('books', \App\Http\Controllers\BookController::class);
